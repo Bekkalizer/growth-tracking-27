53789089c9ca055dd1902fa280cc634d7801e9bd
oyvinege <oyvinege@ifi.uio.no>
2018-01-27 12:35:00 +0200

    removed console logs from referalalert

--- components/growthtracking/components/CirclePage/components/ReferalAlert.jsx
+++ components/growthtracking/components/CirclePage/components/ReferalAlert.jsx
@@ -7,20 +7,15 @@ class ReferalAlert extends React.Component {
 
   checkAlert = (visit) => {
     const message = [];
-    console.log('check visit: ', visit);
     if (visit.wfl < -2) {
-      console.log('check wfl: ', visit.wfl);
       message.push('Weight-for-Length is critically low, refer to district hospital');
     }
     if (visit.wfa < - 2) {
-      console.log('check wfa: ', visit.wfa);
       message.push('Weight-for-Age is critically low, refer to district hospital');
     }
     if (visit.lhfa < -2) {
-      console.log('check wfa: ', visit.lhfa);
       message.push('Length-For-Age is critically low, refer to district hospital');
     } if (visit.muac < -12.5) {
-      console.log('check wfa: ', visit.muac);
       message.push('MUAC is critically low, refer to district hospital');
     }
     return message;
@@ -34,7 +29,7 @@ class ReferalAlert extends React.Component {
 
     const alertMsg = this.checkAlert(visit);
 
-    if (alertMsg !== null) {
+    if (alertMsg.length > 0) {
       return (
         <div className="alert alert-warning">
           <div>
@@ -59,5 +54,16 @@ class ReferalAlert extends React.Component {
     return null;
   }
 }
+
+ReferalAlert.propTypes = {
+  visit: PropTypes.arrayOf(PropTypes.object),
+  toggleAlert: PropTypes.func.isRequired,
+}
+
+ReferalAlert.defaultProps = {
+  visit: [],
+};
+
+
 
 export default ReferalAlert;
